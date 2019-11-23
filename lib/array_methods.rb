64f4b21355b0ba971cb5a852ededc9f62e8a5985
melia miller <meliajm@gmail.com>
2019-11-23 19:56:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,6 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     array.index(value_to_find) if array[index] == value_to_find
   end
-
 end
 
 def find_max_value(array)
