4a79fad7f54977ab4ffb618f0efa3273e5016f28
melia miller <meliajm@gmail.com>
2019-11-23 19:47:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,5 @@
 def find_element_index(array, value_to_find)
-  # array.length.times { |index|
-  # if array[index] == value_to_find
-  #   return array.index(value_to_find)
-  # }
   
-  # else 
-  #   return nil
-  # for i in array {
-  #   if i == value_to_find
-  #     return array.index(value_to_find)
-  # }
-  #   else {
-  #     return nil
-  #   }
-  # array.length.times do | i |  
 
 end
 
