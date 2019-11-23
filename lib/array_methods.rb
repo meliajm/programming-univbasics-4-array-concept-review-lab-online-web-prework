0f83490111fd97b911f5e5eec1e3c139ecb89ef7
melia miller <meliajm@gmail.com>
2019-11-23 20:19:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   array.index(value_to_find) if array.include?(value_to_find)
-  return nil
+  
+  else
+    return nil
 end
 
 def find_max_value(array)
